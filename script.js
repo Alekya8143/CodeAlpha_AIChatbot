26ac87dad17a3b910c07bd1efc206ffedb1a0de1
Alekya <alekyagolla16@gmail.com>
2026-03-05 19:29:19 +0530

    fixed chat bubbles

--- script.js
+++ script.js
@@ -5,25 +5,13 @@ const message = input.value;
 
 const chatBox = document.getElementById("chatBox");
 
-chatBox.innerHTML += `<div class="user-msg"> ${message} </div>`;
+chatBox.innerHTML += `<div class="user-msg">${message}</div>`;
 
-try {
-
-const response = await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${message}&botname=AlekyaBot&ownername=Alekya`);
+const response = await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${message}`);
 
 const data = await response.json();
 
-chatBox.innerHTML += `<div class="bot-msg"> ${data.message} </div>`;
-
-}
-
-catch{
-
-chatBox.innerHTML += `<div class="bot-msg">Error getting response</div>`;
-
-}
+chatBox.innerHTML += `<div class="bot-msg">${data.message}</div>`;
 
 input.value="";
-chatBox.scrollTop = chatBox.scrollHeight;
-
 }
