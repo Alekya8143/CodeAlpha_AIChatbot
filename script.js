e6c4a297542f22a4d5f692bfca46e58a09869640
Alekya <alekyagolla16@gmail.com>
2026-03-05 19:36:59 +0530

    added voice and dark mode

--- script.js
+++ script.js
@@ -5,24 +5,56 @@ let message=input.value;
 
 let messages=document.getElementById("messages");
 
-messages.innerHTML+=`<div class="user">You: ${message}</div><br>`;
+messages.innerHTML+=`<div class="user">${message}</div>`;
 
 try{
 
-let response=await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${message}&botname=AIbot`);
+let response=await fetch(`https://api.affiliateplus.xyz/api/chatbot?message=${message}&botname=AlekyaBot`);
 
 let data=await response.json();
 
-messages.innerHTML+=`<div class="bot">Bot: ${data.message}</div><br>`;
+messages.innerHTML+=`<div class="bot">${data.message}</div>`;
 
 }
 
 catch{
 
-messages.innerHTML+=`<div class="bot">Bot: Error getting response</div><br>`;
+messages.innerHTML+=`<div class="bot">Error getting response</div>`;
 
 }
 
 input.value="";
+
+messages.scrollTop=messages.scrollHeight;
+
+}
+
+
+// voice input
+
+function startVoice(){
+
+let recognition=new webkitSpeechRecognition();
+
+recognition.onresult=function(event){
+
+let text=event.results[0][0].transcript;
+
+document.getElementById("userInput").value=text;
+
+sendMessage();
+
+}
+
+recognition.start();
+
+}
+
+
+// dark mode
+
+function toggleDark(){
+
+document.body.classList.toggle("dark");
 
 }
