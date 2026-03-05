06350f5bdebf06c83c56c4838802bfe1e9f2a8fa
Alekya <alekyagolla16@gmail.com>
2026-03-05 19:07:59 +0530

    Fixed chatbot script

--- script.js
+++ script.js
@@ -17,11 +17,11 @@ reply = "Artificial Intelligence allows machines to simulate human intelligence.
 }
 
 else if(input.includes("machine learning") || input.includes("ml")){
-reply = "Machine learning is a subset of AI that allows computers to learn from data.";
+reply = "Machine learning is a subset of AI where systems learn from data.";
 }
 
 else if(input.includes("deep learning")){
-reply = "Deep learning is a neural network based approach in machine learning.";
+reply = "Deep learning is a type of machine learning that uses neural networks.";
 }
 
 else if(input.includes("python")){
@@ -29,7 +29,7 @@ reply = "Python was created by Guido van Rossum.";
 }
 
 else if(input.includes("react")){
-reply = "React is a JavaScript library used to build user interfaces.";
+reply = "React is a JavaScript library for building user interfaces.";
 }
 
 messages.innerHTML += "<p><b>Bot:</b> " + reply + "</p>";
@@ -37,10 +37,4 @@ messages.innerHTML += "<p><b>Bot:</b> " + reply + "</p>";
 inputBox.value = "";
 messages.scrollTop = messages.scrollHeight;
 
-}
-
-function handleKey(event){
-if(event.key === "Enter"){
-sendMessage();
-}
 }
