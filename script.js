eccb119ec9e7786092f02d334b9f512e8bb113cd
Alekya <alekyagolla16@gmail.com>
2026-03-06 20:14:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,14 +17,11 @@ if(message === "") return;
 
 let messages = document.getElementById("messages");
 
-// User message
 let userMsg = document.createElement("div");
 userMsg.className = "user";
 userMsg.innerText = message;
 messages.appendChild(userMsg);
 
-
-// Typing animation
 let typing = document.createElement("div");
 typing.className = "bot typing";
 typing.innerText = "Typing...";
@@ -32,8 +29,6 @@ messages.appendChild(typing);
 
 messages.scrollTop = messages.scrollHeight;
 
-
-// Bot logic
 let reply = "I am still learning. Ask something about AI or Python.";
 
 let text = message.toLowerCase();
@@ -54,8 +49,6 @@ else if(text.includes("who are you")){
 reply = "I am an AI chatbot created by Alekya.";
 }
 
-
-// Bot reply after delay
 setTimeout(function(){
 
 typing.remove();
@@ -74,8 +67,6 @@ input.value = "";
 
 }
 
-
-// Voice input
 function startVoice(){
 
 let recognition = new webkitSpeechRecognition();
@@ -94,22 +85,18 @@ recognition.start();
 
 }
 
-
-// Dark mode
 function toggleDark(){
 document.body.classList.toggle("dark");
 }
 
 
-// Enter key send message
+
 document.getElementById("userInput").addEventListener("keypress",function(event){
 if(event.key === "Enter"){
 sendMessage();
 }
 });
 
-
-// Clear chat
 function clearChat(){
 document.getElementById("messages").innerHTML="";
 }
